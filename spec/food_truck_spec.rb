6ce94edcac9e85f3ad3ab21700b5658cd82e8a08
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-04-15 10:02:11 -0600

    Finish building tests for It. 1 in FoodTruck class

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -24,33 +24,28 @@ RSpec.describe FoodTruck do
       expect(food_truck.inventory).to eq({})
     end
     
-    it 'can check stock inventory by item' do
-      food_truck = FoodTruck.new("Rocky Mountain Pies")
-      item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
-      item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
-      
-      expect(food_truck.check_stock(item1)).to eq(0)
-    end
   end
+
+  it 'can add stock inventory by item' do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    
+    expect(food_truck.check_stock(item1)).to eq(0)
+    
+    food_truck.stock(item1, 30)
+    
+    expect(food_truck.inventory).to eq({item1 => 30})
+
+    food_truck.stock(item1, 25)
+    
+    expect(food_truck.check_stock(item1)).to eq(55)
+
+    food_truck.stock(item2, 12)
+
+    expect(food_truck.inventory).to eq({item1 => 55, item2 => 12})
+  end
+
 end
 
-#=> 0
-
-pry(main)> food_truck.stock(item1, 30)
-
-pry(main)> food_truck.inventory
-#=> {#<Item:0x007f9c56740d48...> => 30}
-
-pry(main)> food_truck.check_stock(item1)
-#=> 30
-
-pry(main)> food_truck.stock(item1, 25)
-
-pry(main)> food_truck.check_stock(item1)
-#=> 55
-
-pry(main)> food_truck.stock(item2, 12)
-
-pry(main)> food_truck.inventory
-#=> {#<Item:0x007f9c56740d48...> => 55, #<Item:0x007f9c565c0ce8...> => 12}
 
